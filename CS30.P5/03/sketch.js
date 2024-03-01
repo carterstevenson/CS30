eab9522b7982ed1efcfbc13407771301201e044d
carterstevenson <160178712+carterstevenson@users.noreply.github.com>
2024-03-01 15:09:02 -0600

    Add files via upload

---
+++ CS30.P5/03/sketch.js
@@ -0,0 +1,72 @@
+// Gradient Baclground + Nested Loops
+// Carter Stevenson
+// Feb 29th 2024
+// Creating a gradient + drawing with nested loops
+
+let rectHeight = 1;
+let circleSize = 20;
+let spacing = 25;
+
+
+function setup() {  // one time at the start
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+}
+
+function draw() { // over and over, 60 fps
+  gradientBackground();
+  nestedLoops();  // noLoop(); stop at the end of the draw 
+}
+
+/* function mousePressed(){ // this is was for when noLoop was in code
+  draw();
+}
+*/ 
+
+function circleDistance(x1, y1, x2, y2){
+  // given two points (x1, y1), (x2, y2), return the 
+  // straightline distance between. // manhatten??
+  let a = abs(x1-x2);
+  let b = abs(y1-y2);
+  let c = sqrt(pow(a,2) + pow(b,2));
+  return c;
+  // dist();
+
+}
+
+function nestedLoops(){
+  // using a loop iwthhin a loop will generate a grid arrangement
+  // for some circles  
+  for(let x = 0; x < width; x += spacing){ // COLUMN 0 20 40 60 80
+    for (let y = 0; y < height; y += spacing){ // ROWS 0 20 40 60 80
+      let d = round(circleDistance(x, y, mouseX, mouseY));
+      // close circles: different color 
+      let currentSize = circleSize;
+      if(d > 100){
+        fill(0);
+      }
+      else{
+        fill(255, 0, 0);
+        currentSize = circleSize *1.2;
+            }
+      circle (x, y, currentSize);
+      fill(255);
+      textAlign(CENTER, CENTER);
+      text(d, x, y);
+    }
+  }
+}
+
+function gradientBackground(){
+  // use a single loop to draw several rectangles
+  // than color into gradient
+  let y = 0;
+  while (y < height){
+    let c = color(mouseX, map(y,0,height,0,255), map(y,0,height,0,255));
+    fill(c);
+    rect(0, y, width, rectHeight);
+    y += rectHeight;
+  }
+}
+
+
